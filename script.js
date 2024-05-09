6a08cbf4663a1ad20bea3c46e307153861e3e404
Roberto <rob31003@gmail.com>
2024-05-09 19:42:52 -0400

    doc:Conectando con github

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let resultado = document.getElementById('res');
-let resAux;
 let tecla;
 let operaciones=[];
-let botones = document.querySelectorAll('.teclado__tecla');
+const esNum = /^[0-9]*$/;
+
+//por medio de la delegacion de eventos, todos los botones tendran evento click
 let delegEvent = document.querySelector('.calculadora__teclado');
+//Mostrar en pantalla un numero presionado por un botón.
 function numEnPantalla(evento) {
     tecla = evento.target.innerHTML;
     resultado.innerHTML+=tecla;
@@ -13,14 +15,72 @@ function opEnPantalla(evento) {
     operaciones.push(tecla);
     resultado.innerHTML+=tecla;
 }
-function resEnPantalla(evento) {
+function scrnDisplay(evento) {
+    if(evento.target.innerHTML==="DEL"){
+        let caracter = resultado.innerHTML[resultado.innerHTML.length-1];
+        if(operaciones.includes(caracter)){
+            operaciones.pop();
+        }
+        resultado=resultado.sub;
+    }
+    else {
+        operaciones=[];
+        resultado.innerHTML="";
+    }
+}
+//Teclado
+function numEnPantallaTec(keyPressed) {
+     resultado.innerHTML+=keyPressed; 
+}
+function opEnPantallaTec(evento) {
+    tecla = evento.key;
+    operaciones.push(tecla);
+    resultado.innerHTML+=tecla;
+}
+
+//mostrar resultado
+function resEnPantalla() {
     resultado.innerHTML=calcRes();
+    operaciones=[];
+}
+//controlar el teclado
+let tecladoPres = function(e) {
+    if((e.key >= 0 && e.key <= 9) || e.key == "."){
+        numEnPantallaTec(e.key);
+      }
+    //segun el codigo de la tecla, ejecutamos una función
+    switch(e.keyCode){
+        case 187:  // + o *
+            opEnPantallaTec(e);
+            break;
+        case 191:   //dividir(/)
+            opEnPantallaTec(e);
+            break;
+        case 189:   //resta (-)
+            opEnPantallaTec(e);
+            break;
+        case 13:
+            resEnPantalla();
+            break;
+
+        default:
+            break;
+    }
+}
+//Verif si son numeros decimales/flotantes válidos 
+function esNumeroValido(numero) {
+    const regex = /^\d*\.?\d+$/;
+    return regex.test(numero);
 }
 function calcRes() {
     let numerosCad = resultado.innerHTML.split(/[\+\-\*\/]/);
+    if (numerosCad.some(str => !esNumeroValido(str))) {
+        return 'Error: entrada no válida(rev. los numeros)';
+    }
     // return numeros.length;
-    if (numerosCad.length>1) {
+    else if (numerosCad.length>1) {
         let numeros;
+        let resAux;
         if (numerosCad.some(str => str.includes('.'))) {
             numeros = numerosCad.map(str => parseFloat(str));
         } else {
@@ -29,7 +89,8 @@ function calcRes() {
         let contadorOp = 0;
         resAux = numeros[0];
         for(let i=1; i<numeros.length; i++) {
-            switch (operaciones[contadorOp]){
+            if(numeros[i]==0) return 'Error: No es posible dividir entre cero'
+            switch (operaciones[contadorOp]){    
                 case "+": 
                     resAux += numeros[i];
                     break;
@@ -49,26 +110,20 @@ function calcRes() {
         }
         return resAux;
     }   
-} 
+}
+
 delegEvent.addEventListener('click', (e)=> {
-    if(e.target && (e.target).classList.contains('tecla--numero')) {
+    if(e.target && (e.target).classList.contains('tecla--numero') ||
+       e.target && e.target.classList.contains('tecla--punto')) {
         numEnPantalla(e);
     }
     else if(e.target && e.target.classList.contains('tecla--operacion')){
         opEnPantalla(e);
+        
+    } else if(e.target && e.target.classList.contains('tecla--modScrn')) {
+        scrnDisplay(e);
     } else {
-        resEnPantalla(e);
+        resEnPantalla();
     }
 });
-
-
-
-
-
-
-
-// function pantalla(){
-//     let scrn = document.querySelector(".calculadora__pantalla");
-//     let res = scrn.innerText;
-//     alert(res);
-// }
+document.onkeyup = tecladoPres;
